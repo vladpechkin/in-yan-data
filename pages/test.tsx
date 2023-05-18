21ba91f8b84c253ef8360af5e652855ff26d6056
Vlad Pechkin <pechkin.va@tk23.ru>
2023-05-18 13:50:44 +0400

    Add gsheet

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,16 +1,24 @@
 import { overwriteAct } from "@/overwriteAct";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { utils } from "xlsx";
 import { useStore } from "./_app";
 
 const Page = () => {
-  const { acts } = useStore();
+  useEffect(() => {
+    fetch(
+      "https://script.google.com/macros/s/AKfycbxAa5fNUeSo1DxuFHPUvPEl8PNei_hdYftE7q_E3ESUNzq2oZQPIGSXeThR3RjYgBKv/exec",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Basic " + btoa("pechkinde@gmail.com" + ":" + "H4shb4ng.."),
+        },
+      }
+    );
+  }, []);
 
-  const [html, setHtml] = useState("");
-
-  overwriteAct(acts[0], (header) => setHtml(utils.sheet_to_html(header)));
-
-  return <div dangerouslySetInnerHTML={{ __html: html }}></div>;
+  return <div>test</div>;
 };
 
 export default Page;
