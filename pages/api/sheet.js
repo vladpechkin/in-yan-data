d13eae1781508c4966e283bcdfc910c23afcd11c
Vlad <pechkinde@gmail.com>
2023-09-03 23:34:14 +0300

    Fix errors

--- pages/api/sheet.js
+++ pages/api/sheet.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const result = await fetch(
-      "https://script.google.com/macros/s/AKfycbwyRhDB3st9I71SXgTywZhT-r_tBiFwOEzW2v8CjaCzoGEAvAlxBfnKmxYTsOoo4Mqn/exec",
+      "https://script.google.com/macros/s/AKfycbxaGVP4AgwbHA-hWdYhjnrkaujO5dT0AO58zr4EpHTnG1e7VE7Okl8oEnlH60uIusBc/exec",
       {
         method: "POST",
         body: req.body,
