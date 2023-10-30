aa9030bd6ea034627948826f5a667d000b3dc229
Vlad <pechkinde@gmail.com>
2023-10-30 10:14:50 +0400

    Fix errors

--- pages/api/[entityName]/[id].js
+++ pages/api/[entityName]/[id].js
@@ -10,22 +10,17 @@ export default async function handler(req, res) {
       case "GET": {
         const fileData = await fsPromises.readFile(dataFilePath);
         let objectData = JSON.parse(fileData);
-
-
         res.status(200).json(objectData.find((x) => x.id === id));
         break;
       }
       case "PUT": {
         const fileData = await fsPromises.readFile(dataFilePath);
-        let objectData = JSON.parse(fileData);
-        
+        let objectData = JSON.parse(fileData);      
         const body = req.body;
-
         if (Object.keys(body).length === 0) {
           res.status(411);
           return;
         }
-
         objectData = objectData.map((x) => (x.id === id ? body : x));
         console.log(objectData);
         await fsPromises.writeFile(dataFilePath, JSON.stringify(objectData)); 
@@ -35,13 +30,10 @@ export default async function handler(req, res) {
       case "DELETE": {
         const fileData = await fsPromises.readFile(dataFilePath);
         let objectData = JSON.parse(fileData);
-
         const updatedData = JSON.stringify(
           objectData.filter((x) => x.id !== id)
         );
-
         await fsPromises.writeFile(dataFilePath, updatedData);
-
         res.status(200).json("DELETED");
         break;
       }
